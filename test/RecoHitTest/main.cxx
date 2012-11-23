99ae64fe6075108ccecdc534fb59e0e92d87bc0c
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-11-23 19:08:51 +0000

    lots of small improvements
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/RecoHits@838 4ab16672-d4cc-4d65-938a-feac61c82957

--- test/RecoHitTest/main.cxx
+++ test/RecoHitTest/main.cxx
@@ -36,33 +36,38 @@
 #include <WireHit.h>
 #include <WirePointHit.h>
 
+//#define VALGRIND
 
 int main() {
   std::cerr<<"main"<<std::endl;
 
-  const unsigned int nEvents = 500;
+  const unsigned int nEvents = 1000;
   const double BField = 15.;       // kGauss
   const double momentum = 0.5;     // GeV
   const double theta = 110;         // degree
   const double thetaDetPlane = 90;         // degree
   const double phiDetPlane = 0;         // degree
   const double pointDist = 2;      // cm; approx. distance between hits generated w/ RKTrackRep
-  const double pointDistDeg = 2;      // degree; distance between hits generated w/ helix model
+  const double pointDistDeg = 1;      // degree; distance between hits generated w/ helix model
   const double resolution = 0.02;   // cm; resolution of generated hits
 
   const double resolutionWire = 5*resolution;   // cm; resolution of generated hits
-  const TVector3 wireDir(0.,0.,1.);
+  const TVector3 wireDir(0,0,1);
+  const double skewAngle(5);
+  const bool useSkew = false;
+  const int nSuperLayer = 5;
   const double minDrift = 0;
   const double maxDrift = 2;
   const bool idealLRResolution = true; // resolve the l/r ambiguities of the wire hits
 
-  const bool useDaf = false;
+  const bool useDaf = true;
 
   const int pdg = 13;               // particle pdg code
 
   const bool smearPosMom = false;     // init the Reps with smeared pos and mom
-  const double posSmear = 2*resolution;     // cm
-  const double momSmear = 0.05*momentum;     // GeV
+  const double posSmear = 20*resolution;     // cm
+  const double momSmear = 0.1*momentum;     // GeV
+  const double zSmearFac = 100;
   
   const bool HelixTest = true;      // use helix for creating hits
 
@@ -81,32 +86,16 @@ int main() {
 
   hitTypes.push_back(0);
   hitTypes.push_back(0);
-  hitTypes.push_back(0);
-  /*hitTypes.push_back(1);
   hitTypes.push_back(1);
   hitTypes.push_back(1);
   hitTypes.push_back(2);
   hitTypes.push_back(2);
-  hitTypes.push_back(2);
   hitTypes.push_back(3);
   hitTypes.push_back(3);
-  hitTypes.push_back(3);*/
   hitTypes.push_back(4);
   hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-  hitTypes.push_back(4);
-
-  //hitTypes.push_back(5);
+  hitTypes.push_back(5);
+  hitTypes.push_back(5);
 
 
 
@@ -116,13 +105,16 @@ int main() {
   kalman.setNumIterations(3);
 
   GFDaf daf;
+  //daf.setBetas(81.,8.,4.,1.,0.944457, 0.944457);
 
 
-	gRandom->SetSeed(10);
+  gRandom->SetSeed(10);
 
   // init event display
-	GenfitDisplay* display = GenfitDisplay::getInstance();
-	display->reset();
+#ifndef VALGRIND
+  GenfitDisplay* display = GenfitDisplay::getInstance();
+  display->reset();
+#endif
 
   // init geometry and mag. field
   TGeoManager* geom = new TGeoManager("Geometry", "Geane geometry");
@@ -132,12 +124,13 @@ int main() {
 
   const double charge = TDatabasePDG::Instance()->GetParticle(pdg)->Charge()/(3.);
 
-  // init rootapp (for drawing histograms)
-  TApplication* rootapp = new TApplication("rootapp", 0, 0);
 
   // prepare output tree for GFTracks
   GFTrack* trueTrack = new GFTrack();
   GFTrack* fitTrack = new GFTrack();
+#ifndef VALGRIND
+  // init rootapp (for drawing histograms)
+  TApplication* rootapp = new TApplication("rootapp", 0, 0);
   TString outname = "out_Rep";
   outname += "_degPlane";
   outname += ".root";
@@ -148,9 +141,9 @@ int main() {
 
   
   // create histograms
-	gROOT->SetStyle("Plain");
-	gStyle->SetPalette(1);
-	gStyle->SetOptFit(1111);
+  gROOT->SetStyle("Plain");
+  gStyle->SetPalette(1);
+  gStyle->SetOptFit(1111);
   
   TH1D *hmomRes = new TH1D("hmomRes","mom res",2000,-0.01*momentum,0.01*momentum);
   TH1D *hupRes = new TH1D("hupRes","u' res",2000,-0.05,0.05);
@@ -165,11 +158,16 @@ int main() {
   TH1D *huPu = new TH1D("huPu","u pull",200,-6.,6.);
   TH1D *hvPu = new TH1D("hvPu","v pull",200,-6.,6.);
 
+  TH1D *weights = new TH1D("weights","wire Daf vs true weights",500,-1.01,1.01);
+#endif
+
+  double maxWeight(0);
 
   // main loop
   for (unsigned int iEvent=0; iEvent<nEvents; ++iEvent){
       
       if (debug || (iEvent+1)%10==0) std::cout << "Event Nr. " << iEvent+1 << std::endl;
+
 
       // true start values
       TVector3 pos(0, 0, 0);
@@ -202,7 +200,7 @@ int main() {
       if (smearPosMom) {
         posM.SetX(gRandom->Gaus(posM.X(),posSmear));
         posM.SetY(gRandom->Gaus(posM.Y(),posSmear));
-        posM.SetZ(gRandom->Gaus(posM.Z(),posSmear));
+        posM.SetZ(gRandom->Gaus(posM.Z(),zSmearFac*posSmear));
 
         momM.SetX(gRandom->Gaus(momM.X(),momSmear));
         momM.SetY(gRandom->Gaus(momM.Y(),momSmear));
@@ -215,15 +213,19 @@ int main() {
       ((RKTrackRep*)rephits)->setPropDir(1);
 
       if (!matFX) GFMaterialEffects::getInstance()->setNoEffects();
-      					      
-      // remember original initial plane and state					  
+
+      // remember original initial plane and state
       GFDetPlane referencePlane;
       TVectorT<double> referenceState(rephits->getState());
       
       // create smeared hits
       std::vector<GFAbsRecoHit*> hits;
+
+      // true values for left right. 0 for non wire hits
+      std::vector<int> leftRightTrue;
         
       TVector3 point, dir;
+      int wireCounter = 0;
       if (debug) std::cerr << "Start creating hits ... \n";
       try{
         for (unsigned int i=0; i<hitTypes.size(); ++i){
@@ -264,18 +266,34 @@ int main() {
 
           GFAbsRecoHit* hit;
 
+          TVector3 currentWireDir(wireDir);
+
+          if (useSkew && (int)((double)wireCounter/(double)nSuperLayer)%2 == 1) {
+            TVector3 perp(wireDir.Cross(dir));
+            if ((int)((double)wireCounter/(double)nSuperLayer)%4 == 1){
+              currentWireDir.Rotate(skewAngle*TMath::Pi()/180, wireDir.Cross(perp));
+            }
+            else currentWireDir.Rotate(-skewAngle*TMath::Pi()/180, wireDir.Cross(perp));
+          }
+
           TVector3 planeNorm(dir);
           planeNorm.SetTheta(thetaDetPlane*TMath::Pi()/180);
           planeNorm.SetPhi(planeNorm.Phi()+phiDetPlane);
           TVector3 z(0,0,1);
 
           int lr = 1;
-          TVector3 wirePerp = dir.Cross(wireDir);
-          if (gRandom->Uniform(-1,1) > 0) {
-            wirePerp *= -1.;
-            lr = -1;
+          TVector3 wirePerp;
+          if (hitTypes[i] == 4 || hitTypes[i] == 5){
+            wirePerp = dir.Cross(currentWireDir);
+            if (gRandom->Uniform(-1,1) >= 0) {
+              wirePerp *= -1.;
+              lr = -1;
+            }
+            wirePerp.SetMag(gRandom->Uniform(minDrift, maxDrift));
+
+            leftRightTrue.push_back(lr);
           }
-          wirePerp.SetMag(gRandom->Uniform(minDrift, maxDrift));
+          else leftRightTrue.push_back(0);
 
           switch(hitTypes[i]){
             case 0: // 0: PixHit
@@ -287,7 +305,7 @@ int main() {
               break;
 
             case 2: // 2: ProlateSpacepointHit
-              hit = new ProlateSpacepointHit(point, wireDir, resolution, resolutionWire, true);
+              hit = new ProlateSpacepointHit(point, currentWireDir, resolution, resolutionWire, true);
               break;
 
             case 3: // 3: StripHit
@@ -295,17 +313,19 @@ int main() {
               break;
 
             case 4: // 4: WireHit
-              hit = new WireHit(point-wirePerp, point-wirePerp+wireDir, wirePerp.Mag(), resolution, true);
+              hit = new WireHit(point-wirePerp-currentWireDir, point-wirePerp+currentWireDir, wirePerp.Mag(), resolution, true);
               if (idealLRResolution){
                 ((WireHit*)hit)->setLeftRightResolution(lr);
               }
+              ++wireCounter;
               break;
 
             case 5: // 5: WirePointHit
-              hit = new WirePointHit(point-wirePerp, point-wirePerp+wireDir, wirePerp.Mag(), 0, resolution, resolutionWire, true);
+              hit = new WirePointHit(point-wirePerp-currentWireDir, point-wirePerp+currentWireDir, wirePerp.Mag(), currentWireDir.Mag(), resolution, resolutionWire, true);
               if (idealLRResolution){
                 ((WireHit*)hit)->setLeftRightResolution(lr);
               }
+              ++wireCounter;
               break;
 
             default:
@@ -323,11 +343,13 @@ int main() {
             rephits->extrapolate(pl);
           }
         }
+
+        assert(hitTypes.size() == leftRightTrue.size());
       }
       catch(GFException& e){
-	      std::cerr<<"Exception, next track"<<std::endl;
-	      std::cerr<< e.what();
-	      continue; // here is a memleak!
+        std::cerr<<"Exception, next track"<<std::endl;
+        e.what();
+        continue; // here is a memleak!
       } 
       
       if (debug) std::cerr << "... done creating hits \n";
@@ -337,25 +359,25 @@ int main() {
       // trackrep to be fitted and tested
       GFAbsTrackRep* rep;
       rep = new RKTrackRep(posM, momM, posErr, momErr, pdg);
-      				          
-		  // create track
+
+      // create track
       if (fitTrack != NULL) delete fitTrack;
-		  fitTrack = new GFTrack(rep); //initialized with smeared rep
-		  fitTrack->setSmoothing(smoothing);
-		  
-		  // add hits
+      fitTrack = new GFTrack(rep); //initialized with smeared rep
+      fitTrack->setSmoothing(smoothing);
+
+      // add hits
       for(unsigned int i=0; i<hits.size(); ++i){
         fitTrack->addHit(hits[i],
                          hitTypes[i], //detector id
-		                     i); // hit id
+                         i); // hit id
       }
 
       // print trackCand
       if (debug) fitTrack->getCand().Print();
 
 
-		  
-		  // do the fit
+
+      // do the fit
       try{
         if (useDaf) {
           if (debug) std::cerr<<"Starting the fitter (Daf)"<<std::endl;
@@ -369,8 +391,8 @@ int main() {
       }
       catch(GFException& e){
         e.what();
-	      std::cerr<<"Exception, next track"<<std::endl;
-	      continue;
+        std::cerr<<"Exception, next track"<<std::endl;
+        continue;
       }
 
 
@@ -379,91 +401,134 @@ int main() {
         continue;
       }
       
-
+#ifndef VALGRIND
       // add track to event display
       std::vector<GFTrack*> event;
-			event.push_back(fitTrack);
-			display->addEvent(event);
+      event.push_back(fitTrack);
+      display->addEvent(event);
+#endif
+
+      if (debug) {
+        std::cout << "cov before extrapolating back to reference plane \n";
+        rep->getCov().Print();
+
+        std::cout << "pos before extrapolating back to reference plane \n";
+        rep->getPos().Print();
+      }
+
+      // extrapolate back to reference plane.
+      try{
+        rep->extrapolate(referencePlane);
+      }
+      catch(GFException& e){
+        std::cerr<<"Exception, next track"<<std::endl;
+        e.what();
+        continue; // here is a memleak!
+      }
 
 
-			if (debug) {
-			  std::cout << "cov before extrapolating back to reference plane \n";
-			  rep->getCov().Print();
-			  
-			  std::cout << "pos before extrapolating back to reference plane \n";
-			  rep->getPos().Print();
-			}
-				          
-		  // extrapolate back to reference plane.
-		  rep->extrapolate(referencePlane);
-		  
-
-		  // check getPos etc methods
-		  const double epsilon = 1E-3;
-		  TVector3 getPos, getMom;
+      // check getPos etc methods
+      const double epsilon = 1E-3;
+      TVector3 getPos, getMom;
 
 
-		  // calculate pulls  
-		  TVectorT<double> state(rep->getState());
-		  TMatrixTSym<double> cov(rep->getCov());
-		  
-		  if (debug) {
-        state.Print();
-        cov.Print();
-		  }
+      // calculate pulls
+      TVectorT<double> state(rep->getState());
+      TMatrixTSym<double> cov(rep->getCov());
 
+      if (debug) {
+        fitTrack->getBK()->Print();
+      }
+
+#ifndef VALGRIND
       hmomRes->Fill( (charge/state[0]-momentum));
       hupRes->Fill(  (state[1]-referenceState[1]));
       hvpRes->Fill(  (state[2]-referenceState[2]));
       huRes->Fill(   (state[3]-referenceState[3]));
       hvRes->Fill(   (state[4]-referenceState[4]));
 
-		  if (cov[0][0]>0) {
-		    hqopPu->Fill( (state[0]-referenceState[0]) / sqrt(cov[0][0]) );
+      if (cov[0][0]>0) {
+        hqopPu->Fill( (state[0]-referenceState[0]) / sqrt(cov[0][0]) );
         pVal->Fill(   rep->getPVal());
         hupPu->Fill(  (state[1]-referenceState[1]) / sqrt(cov[1][1]) );
         hvpPu->Fill(  (state[2]-referenceState[2]) / sqrt(cov[2][2]) );
         huPu->Fill(   (state[3]-referenceState[3]) / sqrt(cov[3][3]) );
         hvPu->Fill(   (state[4]-referenceState[4]) / sqrt(cov[4][4]) );
-		  }
-		  
-		  // print covariance
-			if (debug) cov.Print();
-			else if((iEvent)%100==0)  cov.Print();
-				          
-
-		  if (debug && smoothing){
-		  	std::cout << "smoothed positions \n";
-		  	GFDetPlane plane;
-			  for(unsigned int j = 0; j < hitTypes.size(); j++) { // loop over all hits in the track
-					TVectorT<double> state;
-				  TMatrixTSym<double> cov;
-				  TMatrixT<double> auxInfo;
-				  GFTools::getSmoothedData(fitTrack, 0, j, state, cov, plane, auxInfo);
-				  rep->setData(state, plane, &cov, &auxInfo);
-			    rep->getPos(plane).Print();
-			  }
       }
 
-		  if (debug) std::cerr<<"Fill Tree ..." << std::endl;
-			tree->Fill();
+      // print covariance
+      if (debug) cov.Print();
+      else if((iEvent)%100==0)  cov.Print();
 
-				          
-				          
+
+
+      // check l/r resolution
+      if (smoothing && useDaf) {
+        for (unsigned int i=0; i<leftRightTrue.size(); ++i){
+          int trueSide = leftRightTrue[i];
+          if (trueSide == 0) continue; // not a wire hit
+          const TVectorT<double>& dafWeightLR = fitTrack->getBK(0)->getVector(GFBKKey_dafWeight, i);
+          assert(dafWeightLR.GetNrows()==2);
+
+          double weightCorrectSide, weightWrongSide;
+
+          if (trueSide < 0) {
+            weightCorrectSide = dafWeightLR[0];
+            weightWrongSide =  dafWeightLR[1];
+          }
+          else {
+            weightCorrectSide = dafWeightLR[1];
+            weightWrongSide =  dafWeightLR[0];
+          }
+          weightWrongSide -= 1.;
+
+          weights->Fill(weightCorrectSide);
+          weights->Fill(weightWrongSide);
+
+          if (weightCorrectSide>maxWeight) maxWeight = weightCorrectSide;
+        }
+
+      }
+
+
+      if (debug && smoothing){
+        std::cout << "smoothed positions \n";
+        GFDetPlane plane;
+        for(unsigned int j = 0; j < hitTypes.size(); j++) { // loop over all hits in the track
+          TVectorT<double> state;
+          TMatrixTSym<double> cov;
+          TMatrixT<double> auxInfo;
+          GFTools::getBiasedSmoothedData(fitTrack, 0, j, state, cov, plane, auxInfo);
+          rep->setData(state, plane, &cov, &auxInfo);
+          rep->getPos(plane).Print();
+        }
+      }
+
+      if (debug) std::cerr<<"Fill Tree ..." << std::endl;
+      tree->Fill();
+#endif
+
+
   }// end loop over events
+
+  std::cerr<<"maxWeight = " << maxWeight << std::endl;
+
 #ifndef VALGRIND
   if (debug) std::cerr<<"Write Tree ...";
   tree->Write();
   if (debug) std::cerr<<"... done"<<std::endl;
 
   if (debug) std::cerr<<"Draw histograms ...";
-	// fit and draw histograms
+  // fit and draw histograms
   TCanvas* c1 = new TCanvas();
   c1->Divide(2,3);
 
   c1->cd(1);
   hmomRes->Fit("gaus");
   hmomRes->Draw();
+
+  c1->cd(2);
+  weights->Draw();
 
   c1->cd(3);
   hupRes->Fit("gaus");
@@ -483,44 +548,46 @@ int main() {
 
   c1->Write();
 
-	TCanvas* c2 = new TCanvas();
-	c2->Divide(2,3);
-	
-	c2->cd(1);
-	hqopPu->Fit("gaus");
-	hqopPu->Draw();
-	
-	c2->cd(2);
-	pVal->Fit("pol1");
-	pVal->Draw();
-	c2->cd(3);
-	hupPu->Fit("gaus");
-	hupPu->Draw();
-		
-	c2->cd(4);
-	hvpPu->Fit("gaus");
-	hvpPu->Draw();
-		
-	c2->cd(5);
-	huPu->Fit("gaus");
-	huPu->Draw();	
-		
-	c2->cd(6);
-	hvPu->Fit("gaus");
-	hvPu->Draw();
-	
-	c2->Write();
+  TCanvas* c2 = new TCanvas();
+  c2->Divide(2,3);
 
-	if (debug) std::cerr<<"... done"<<std::endl;
-	
-	// open event display
-	display->setOptions("THDPMA"); // G show geometry
-	display->open();
-	
+  c2->cd(1);
+  hqopPu->Fit("gaus");
+  hqopPu->Draw();
+
+  c2->cd(2);
+  pVal->Fit("pol1");
+  pVal->Draw();
+  c2->cd(3);
+  hupPu->Fit("gaus");
+  hupPu->Draw();
+
+  c2->cd(4);
+  hvpPu->Fit("gaus");
+  hvpPu->Draw();
+
+  c2->cd(5);
+  huPu->Fit("gaus");
+  huPu->Draw();
+
+  c2->cd(6);
+  hvPu->Fit("gaus");
+  hvPu->Draw();
+
+  c2->Write();
+
+  if (debug) std::cerr<<"... done"<<std::endl;
+
+  // open event display
+  display->setOptions("THDPMA"); // G show geometry
+  display->open();
+
   rootapp->Run(); 
 
-	file->Close();
-  if (debug) std::cerr<<"... closed file"<<std::endl;
+  file->Close();
 #endif
+
+  if (debug) std::cerr<<"... closed file"<<std::endl;
+
 }
 
