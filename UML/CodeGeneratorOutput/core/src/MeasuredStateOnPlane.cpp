3c97a6fbf416368e8355cfe16c37ee00c0227a34
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2013-04-02 11:12:36 +0000

    implementation of MeasuredStateOnPlane
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/genfit2@950 4ab16672-d4cc-4d65-938a-feac61c82957

--- UML/CodeGeneratorOutput/core/src/MeasuredStateOnPlane.cpp
+++ UML/CodeGeneratorOutput/core/src/MeasuredStateOnPlane.cpp
@@ -3,8 +3,22 @@
 namespace genfit {
 
 
-  /** 
-   *  Additional covariance matrix.
-   */
+  MeasuredStateOnPlane::MeasuredStateOnPlane() :
+    StateOnPlane(), cov_(0,0)
+  {
+    ;
+  }
+
+  MeasuredStateOnPlane::MeasuredStateOnPlane(const TVectorD& state, const TMatrixDSym& cov, DetPlane* plane, AbsTrackRep* rep) :
+    StateOnPlane(state, plane, rep), cov_(cov)
+  {
+    ;
+  }
+
+  MeasuredStateOnPlane::MeasuredStateOnPlane(const StateOnPlane& state, const TMatrixDSym& cov) :
+    StateOnPlane(state), cov_(cov)
+  {
+    ;
+  }
 
 } /* End of namespace genfit */
