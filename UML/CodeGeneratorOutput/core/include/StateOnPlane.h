83b0eecb98deeda3b643208ab8669054c11c79d5
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2013-04-02 11:09:47 +0000

    implementation of StateOnPlane
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/genfit2@949 4ab16672-d4cc-4d65-938a-feac61c82957

--- UML/CodeGeneratorOutput/core/include/StateOnPlane.h
+++ UML/CodeGeneratorOutput/core/include/StateOnPlane.h
@@ -1,3 +1,26 @@
+/* Copyright 2008-2010, Technische Universitaet Muenchen,
+   Authors: Christian Hoeppner & Sebastian Neubert & Johannes Rauch
+
+   This file is part of GENFIT.
+
+   GENFIT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   GENFIT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with GENFIT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** @addtogroup genfit
+ * @{
+ */
+
 #ifndef genfit_StateOnPlane_h
 #define genfit_StateOnPlane_h
 
@@ -16,24 +39,31 @@ namespace genfit {
    *  A state with arbitrary dimension defined in a #GFDetPlane. #fSharedPlane is a shared_pointer, the ownership over that plane is shared between all #GFStateOnPlane objects defined in that plane.
    *  
    *  The definition of the state is bound to the TrackRep. Therefore, the #GFStateOnPlane contains a pointer to a #GFAbsTrackRep. It will provide functionality to extrapolate it and translate the state it into cartesian coordinates. 
-   *  If the #GFStateOnPlane is calculated from a #GFAbsRawMeasurement, it does in general not have the full dimensionality and can therefore not be extrapolated in general.
    */
 class StateOnPlane {
 
  public:
 
   StateOnPlane();
-  StateOnPlane(DetPlane* plane, const TVectorD& state);
+  StateOnPlane(const TVectorD& state, DetPlane* plane, AbsTrackRep* rep);
 
+  ~StateOnPlane();
+
+  const TVectorD& getState() const {return state_;}
+  const DetPlane* getPlane() const {return sharedPlane_;}
+  const AbsTrackRep* getRep() const {return rep_;}
 
  protected:
+
+  void setState(const TVectorD& state) {state_ = state;}
+  void setStatePlane(const TVectorD& state, DetPlane* plane);
+
+ protected:
+
   TVectorD state_;
+  DetPlane* sharedPlane_; // Ownership. TODO: Change to std::shared_ptr when changing to ROOT 6
+  AbsTrackRep* rep_; // No ownership
 
- public:
-
-  DetPlane* sharedPlane_;
-
-  AbsTrackRep* rep_;
 };
 
 } /* End of namespace genfit */
--- UML/CodeGeneratorOutput/core/src/StateOnPlane.cpp
+++ UML/CodeGeneratorOutput/core/src/StateOnPlane.cpp
@@ -1,15 +1,51 @@
+/* Copyright 2008-2010, Technische Universitaet Muenchen,
+   Authors: Christian Hoeppner & Sebastian Neubert & Johannes Rauch
+
+   This file is part of GENFIT.
+
+   GENFIT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   GENFIT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with GENFIT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "StateOnPlane.h"
 
 namespace genfit {
 
+  StateOnPlane::StateOnPlane() :
+    state_(0), sharedPlane_(NULL), rep_(NULL)
+  {
+    ;
+  }
 
-  /** 
-   *  A state with arbitrary dimension defined in a #GFDetPlane. #fSharedPlane is a shared_pointer, the ownership over that plane is shared between all #GFStateOnPlane objects defined in that plane.
-   *  
-   *  The definition of the state is bound to the TrackRep. Therefore, the #GFStateOnPlane contains a pointer to a #GFAbsTrackRep. It will provide functionality to extrapolate it and translate the state it into cartesian coordinates. 
-   *  If the #GFStateOnPlane is calculated from a #GFAbsRawMeasurement, it does in general not have the full dimensionality and can therefore not be extrapolated in general.
-   */
+  StateOnPlane::StateOnPlane(const TVectorD& state, DetPlane* plane, AbsTrackRep* rep) :
+    state_(state), sharedPlane_(plane), rep_(rep)
+  {
+    ;
+  }
 
 
+  StateOnPlane::~StateOnPlane() {
+    if (sharedPlane_ != NULL)
+      delete sharedPlane_;
+  }
+
+
+  void
+  StateOnPlane::setStatePlane(const TVectorD& state, DetPlane* plane) {
+    state_ = state;
+    if (sharedPlane_ != NULL)
+      delete sharedPlane_;
+    sharedPlane_ = plane;
+  }
 
 } /* End of namespace genfit */
