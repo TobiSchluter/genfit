c6ad5e1c3a70e687cb76687db570879e8f607ac8
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2013-07-10 11:29:31 +0000

    fix a set of bugs in inActive functions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/genfit2@1227 4ab16672-d4cc-4d65-938a-feac61c82957

--- core/include/DetPlane.h
+++ core/include/DetPlane.h
@@ -145,14 +145,14 @@ class DetPlane { //: public TObject {
 
   //! intersect in the active area? C.f. AbsFinitePlane
   bool isInActive(const TVector3& point, const TVector3& dir) const {
-    if(finitePlane_.get() != NULL) return true;
+    if(finitePlane_.get() == NULL) return true;
     return this->isInActive( this->straightLineToPlane(point,dir));
   }
 
   //! intersect in the active area? C.f. AbsFinitePlane
   bool isInActive(const double& posX, const double& posY, const double& posZ,
                   const double& dirX, const double& dirY, const double& dirZ) const {
-    if(finitePlane_.get() != NULL) return true;
+    if(finitePlane_.get() == NULL) return true;
     double u, v;
     this->straightLineToPlane(posX, posY, posZ, dirX, dirY, dirZ, u, v);
     return this->isInActive(u, v);
@@ -160,7 +160,7 @@ class DetPlane { //: public TObject {
 
   //! isInActive methods refer to finite plane. C.f. AbsFinitePlane
   bool isInActive(double u, double v) const{
-    if(finitePlane_.get() != NULL) return true;
+    if(finitePlane_.get() == NULL) return true;
     return finitePlane_->isInActive(u,v);
   }
 
