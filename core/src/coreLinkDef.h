22eed8420bd8c73ba51c8a49febbe22e1d922f8b
innenminister <innenminister@4ab16672-d4cc-4d65-938a-feac61c82957>
2013-07-16 19:37:14 +0000

    Sort coreLinkDef.h the sme way as the other LinkDefs, explain in comments.
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/genfit2@1262 4ab16672-d4cc-4d65-938a-feac61c82957

--- core/src/coreLinkDef.h
+++ core/src/coreLinkDef.h
@@ -4,21 +4,24 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class genfit::DetPlane-;  // scoped_ptr<> finitePlane_
+// These need no special treatment.
 #pragma link C++ class genfit::AbsFinitePlane;
 #pragma link C++ class genfit::RectangularFinitePlane;
 #pragma link C++ class genfit::AbsTrackRep;
-
-#pragma link C++ class genfit::StateOnPlane-;  // rep_, sharedPlanePtr
 #pragma link C++ class genfit::MeasurementOnPlane;
 #pragma link C++ class genfit::MeasuredStateOnPlane;
+#pragma link C++ class genfit::FitStatus;
 
+// These need their owners fixed up after reading.
 #pragma link C++ class genfit::AbsMeasurement; // trackPoint_
 #pragma link C++ class genfit::AbsFitterInfo; // trackPoint_, rep_
 
-#pragma link C++ class genfit::TrackPoint-; // track_, fixup the map
-#pragma link C++ class genfit::FitStatus;
-
+// These cannot be dealt with by default streamers because of
+// shared_ptrs<> or scoped_ptrs<>.  Additionally, they may need their
+// owners fixed up.
+#pragma link C++ class genfit::DetPlane-;  // scoped_ptr<> finitePlane_
+#pragma link C++ class genfit::StateOnPlane-;  // rep_, sharedPlanePtr
 #pragma link C++ class genfit::Track-;
+#pragma link C++ class genfit::TrackPoint-; // track_, fixup the map
 
 #endif
