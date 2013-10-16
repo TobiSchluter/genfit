1df25f0f1cd24653f6a2d36fbc9fb0fa49f108af
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2013-10-16 10:37:13 +0000

    new measurements class which measures all track parameters, can be used e.g. to combine fitted state of a subdetector into a single measurement. Not yet tested.
    git-svn-id: svn+ssh://svn.code.sf.net/p/genfit/code/branches/genfit2@1560 4ab16672-d4cc-4d65-938a-feac61c82957

--- measurements/src/measurementsLinkDef.h
+++ measurements/src/measurementsLinkDef.h
@@ -4,8 +4,8 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class genfit::HMatrixPhi-;
 #pragma link C++ class genfit::HMatrixU+;
+#pragma link C++ class genfit::HMatrixUnit+;
 #pragma link C++ class genfit::HMatrixV+;
 #pragma link C++ class genfit::HMatrixUV+;
 #pragma link C++ class genfit::ProlateSpacepointMeasurement+;
@@ -13,6 +13,8 @@
 #pragma link C++ class genfit::WireMeasurement+;
 #pragma link C++ class genfit::WirePointMeasurement+;
 
+#pragma link C++ class genfit::HMatrixPhi-;
+#pragma link C++ class genfit::FullMeasurement-;
 #pragma link C++ class genfit::PlanarMeasurement-;
 
 #endif
