43b5e908ffc9087bcd6a40151dff8eeea339c74d
Tobias Schlüter <tobi@schlueters.de>
2015-08-27 10:38:16 +0200

    Field of in test, more output, more variations.

--- test/compare/main.cc
+++ test/compare/main.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <AbsFinitePlane.h>
 #include <AbsFitterInfo.h>
 #include <AbsMeasurement.h>
@@ -39,31 +41,7 @@
 #include <StepLimits.h>
 #include <TGeoMaterialInterface.h>
 
-#include <EventDisplay.h>
-
-#include <HelixTrackModel.h>
-#include <MeasurementCreator.h>
-
-#include <TApplication.h>
-#include <TCanvas.h>
-#include <TDatabasePDG.h>
-#include <TEveManager.h>
 #include <TGeoManager.h>
-#include <TH1D.h>
-#include <TRandom.h>
-#include <TStyle.h>
-#include <TVector3.h>
-#include <vector>
-
-#include <TROOT.h>
-#include <TFile.h>
-#include <TTree.h>
-#include "TDatabasePDG.h"
-#include <TMath.h>
-#include <TString.h>
-
-#include <memory>
-#include <getopt.h>
 
 void setup() {
   // gStyle->SetCanvasPreferGL(true);
@@ -78,6 +56,7 @@ void setup() {
 
   //--- define the transformations
   TGeoTranslation *tr1 = new TGeoTranslation(0., 0, 0.);
+  TGeoTranslation *tr2 = new TGeoTranslation(0., .5, 0.);
 
   //--- make the top container volume
   Double_t worldx = 100.;
@@ -87,9 +66,10 @@ void setup() {
   geom->SetTopVolume(top);
   TGeoVolume *box = geom->MakeBox("BOX", Al, worldx, .1, worldz);
   top->AddNode(box, 1, tr1);
+  top->AddNode(box, 2, tr2);
   geom->CloseGeometry();
    
-  genfit::FieldManager::getInstance()->init(new genfit::ConstField(0.,0.,15));
+  genfit::FieldManager::getInstance()->init(new genfit::ConstField(0.,0.,0.));
   genfit::MaterialEffects::getInstance()->init(new genfit::TGeoMaterialInterface());
 }
 
@@ -106,17 +86,58 @@ int main()
   StateOnPlane sop2(rk2);
 
   TVector3 pos(0, -1, 0);
-  TVector3 mom(0, 1, 0);
+  TVector3 mom(0, .1, 0);
+  SharedPlanePtr start(new DetPlane(TVector3(0, -1, 0), TVector3(0, 1, 0)));
   sop1.setPosMom(pos, mom);
   sop2.setPosMom(pos, mom);
 
+  StateOnPlane sop3(sop1);
+  StateOnPlane sop4(sop2);
+
   SharedPlanePtr target(new DetPlane(TVector3(0, 1, 0), TVector3(0, 1, 0)));
+  SharedPlanePtr middle(new DetPlane(TVector3(0, .2, 0), TVector3(0, 1, 0)));
+
+  std::cout << "forth ->->->->->->->->->->->->->->->->->" << std::endl;
+
   sop1.extrapolateToPlane(target);
   sop1.Print();
 
   //rk2->setDebugLvl(1);
   sop2.extrapolateToPlane(target);
   sop2.Print();
+
+  std::cout << "back <-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-" << std::endl;
+
+  sop1.extrapolateToPlane(start);
+  sop1.Print();
+
+  //rk2->setDebugLvl(1);
+  sop2.extrapolateToPlane(start);
+  sop2.Print();
+
+  std::cout << "forth in two steps -->-->-->-->-->-->-->-->" << std::endl;
+
+  sop3.extrapolateToPlane(middle);
+  //sop3.Print();
+  sop3.extrapolateToPlane(target);
+  sop3.Print();
+
+  sop4.extrapolateToPlane(middle);
+  //sop4.Print();
+  sop4.extrapolateToPlane(target);
+  sop4.Print();
+
+  std::cout << "back in two steps <--<--<--<--<--<--<--<--" << std::endl;
+
+  sop3.extrapolateToPlane(middle);
+  //sop3.Print();
+  sop3.extrapolateToPlane(start);
+  sop3.Print();
+
+  sop4.extrapolateToPlane(middle);
+  //sop4.Print();
+  sop4.extrapolateToPlane(start);
+  sop4.Print();
 
   return 0;
 }
