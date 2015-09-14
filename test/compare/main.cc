192465ca4f2fb4d1b688254e761f7de6d7033382
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2015-09-14 13:21:50 +0200

    tests/compare: Some more work.

--- test/compare/main.cc
+++ test/compare/main.cc
@@ -50,12 +50,11 @@ void setup() {
   //--- define some materials
   TGeoMaterial *matVacuum = new TGeoMaterial("Vacuum", 0,0,0);
   TGeoMaterial *matAl = new TGeoMaterial("Al", 26.98,13,2.7);
-  TGeoMaterial *matAlLowRho = new TGeoMaterial("Al", 26.98,13,0.27);
+  TGeoMaterial *matAir = new TGeoMaterial("Air", 14.61, 7.3, 0.1205e-2);
   //   //--- define some media
   TGeoMedium *Vacuum = new TGeoMedium("Vacuum",1, matVacuum);
   TGeoMedium *Al = new TGeoMedium("Al",2, matAl);
-  TGeoMedium *AlLowRho = new TGeoMedium("AlLowRho",3, matAl);
-
+  TGeoMedium *Air = new TGeoMedium("Air", 1, matAir);
   //--- define the transformations
   TGeoTranslation *tr1 = new TGeoTranslation(0., 0, 0.);
   TGeoTranslation *tr2 = new TGeoTranslation(0., .5, 0.);
@@ -67,14 +66,14 @@ void setup() {
   Double_t worldx = 100.;
   Double_t worldy = 100.;
   Double_t worldz = 100.;
-  TGeoVolume *top = geom->MakeBox("TOP", Vacuum, worldx, worldy, worldz);
+  TGeoVolume *top = geom->MakeBox("TOP", Air, worldx, worldy, worldz);
   geom->SetTopVolume(top);
   TGeoVolume *box = geom->MakeBox("AlBOX", Al, worldx, .01, worldz);
   top->AddNode(box, 1, tr1);
   top->AddNode(box, 2, tr2);
   TGeoVolume *smallBox = geom->MakeBox("smallBox", Al, worldx, .001, worldz);
   top->AddNode(smallBox, 1, tr3);
-  TGeoVolume *smallBox2 = geom->MakeBox("smallBoxLow", AlLowRho, worldx, .001, worldz);
+  TGeoVolume *smallBox2 = geom->MakeBox("smallBoxLow", Air, worldx, .001, worldz);
   top->AddNode(smallBox2, 1, tr4);
   top->AddNode(smallBox, 2, tr5);
 
@@ -93,89 +92,113 @@ int main()
   RKTrackRep* rk1 = new RKTrackRep(-211);
   RKTrackRepEnergy* rk2 = new RKTrackRepEnergy(-211);
 
-  MeasuredStateOnPlane sop1(rk1);
-  MeasuredStateOnPlane sop2(rk2);
+  MeasuredStateOnPlane mop1(rk1);
+  MeasuredStateOnPlane mop2(rk2);
 
   TVector3 pos(0, -2, 0);
-  TVector3 mom(0.001, .1, 0.01);
+  TVector3 mom(0, .1, .0);
   TMatrixDSym cov(6);
   for (int i = 0; i < 6; ++i)
     cov(i, i) = 1.e-2;
   SharedPlanePtr start(new DetPlane(TVector3(0, -2, 0), TVector3(0, 1, 0)));
-  sop1.setPosMomCov(pos, mom, cov);
-  sop2.setPosMomCov(pos, mom, cov);
+  mop1.setPosMomCov(pos, mom, cov);
+  mop2.setPosMomCov(pos, mom, cov);
 
-  MeasuredStateOnPlane sop3(sop1);
-  MeasuredStateOnPlane sop4(sop2);
+  MeasuredStateOnPlane mop3(mop1);
+  MeasuredStateOnPlane mop4(mop2);
 
-  SharedPlanePtr target(new DetPlane(TVector3(0, 1, 0), TVector3(0.1, 1, 0.1)));
-  SharedPlanePtr middle(new DetPlane(TVector3(0, .2, 0), TVector3(0, 1, 0)));
+  SharedPlanePtr target(new DetPlane(TVector3(0, 1, 0), TVector3(0, 1, .2)));
+  SharedPlanePtr middle(new DetPlane(TVector3(0, .02, 0), TVector3(0, 1, 0)));
 
   std::cout << "forth ->->->->->->->->->->->->->->->->->" << std::endl;
 
-  sop1.extrapolateToPlane(target);
-  sop1.Print();
+  mop1.extrapolateToPlane(target);
+  mop1.Print();
   TMatrixD jac(5,5);
   TMatrixDSym noise(5);
   TVectorD d(5);
   std::cout << "analytically RKTrackRep" << std::endl;
   rk1->getForwardJacobianAndNoise(jac, noise, d);
   jac.Print();
-  noise.Print();
   jac.Zero();
   std::cout << "numerically RKTrackRep:" << std::endl;
-  rk1->calcJacobianNumerically(sop3, target, jac);
+  rk1->calcJacobianNumerically(mop3, target, jac);
   jac.Print();
 
   std::cout << "next" << std::endl;
 
   //rk2->setDebugLvl(1);
-  sop2.extrapolateToPlane(target);
-  sop2.Print();
+  mop2.extrapolateToPlane(target);
+  mop2.Print();
   std::cout << "analytically RKTrackRepEnergy" << std::endl;
-  rk2->getForwardJacobianAndNoise(jac, noise, d);
-  jac.Print();
-  noise.Print();
+  TMatrixD jacf(5,5);
+  rk2->getForwardJacobianAndNoise(jacf, noise, d);
+  jacf.Print();
+  TMatrixD jacfn(5,5);
   jac.Zero();
   std::cout << "numerically RKTrackRepEnergy:" << std::endl;
-  rk2->calcJacobianNumerically(sop4, target, jac);
+  rk2->calcJacobianNumerically(mop4, target, jacfn);
   std::cout << " jac " << std::endl;
-  jac.Print();
+  jacfn.Print();
 
-  return 0;
+  //return 0;
 
   std::cout << "back <-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-" << std::endl;
 
-  sop1.extrapolateToPlane(start);
-  sop1.Print();
+  MeasuredStateOnPlane mop1b(mop1);
+  mop1b.extrapolateToPlane(start);
+  mop1b.Print();
 
-  //rk2->setDebugLvl(1);
-  sop2.extrapolateToPlane(start);
-  sop2.Print();
+  std::cout << "analytically RKTrackRep" << std::endl;
+  rk1->getForwardJacobianAndNoise(jac, noise, d);
+  jac.Print();
+  jac.Zero();
+  std::cout << "numerically RKTrackRep:" << std::endl;
+  rk1->calcJacobianNumerically(mop1, start, jac);
+  jac.Print();
 
+  MeasuredStateOnPlane mop2b(mop2);
+  mop2b.extrapolateToPlane(start);
+  mop2b.Print();
+  TMatrixD jacb(5,5);
+  std::cout << "analytically RKTrackRepE" << std::endl;
+  rk2->getForwardJacobianAndNoise(jacb, noise, d);
+  jacb.Print();
+  TMatrixD jacbn(5,5);
+  std::cout << "numerically RKTrackRepE:" << std::endl;
+  rk2->calcJacobianNumerically(mop2, start, jacbn);
+  jacbn.Print();
+
+  std::cout << "product Jfw Jbw analytically" << std::endl;
+  TMatrixD(jacb, TMatrixD::kMult, jacf).Print();
+  std::cout << "product Jfw Jbw numerically" << std::endl;
+  TMatrixD(jacbn, TMatrixD::kMult, jacfn).Print();
+
+
+  return(0);
   std::cout << "forth in two steps -->-->-->-->-->-->-->-->" << std::endl;
 
-  sop3.extrapolateToPlane(middle);
-  //sop3.Print();
-  sop3.extrapolateToPlane(target);
-  sop3.Print();
+  mop3.extrapolateToPlane(middle);
+  //mop3.Print();
+  mop3.extrapolateToPlane(target);
+  mop3.Print();
 
-  sop4.extrapolateToPlane(middle);
-  //sop4.Print();
-  sop4.extrapolateToPlane(target);
-  sop4.Print();
+  mop4.extrapolateToPlane(middle);
+  //mop4.Print();
+  mop4.extrapolateToPlane(target);
+  mop4.Print();
 
   std::cout << "back in two steps <--<--<--<--<--<--<--<--" << std::endl;
 
-  sop3.extrapolateToPlane(middle);
-  //sop3.Print();
-  sop3.extrapolateToPlane(start);
-  sop3.Print();
+  mop3.extrapolateToPlane(middle);
+  //mop3.Print();
+  mop3.extrapolateToPlane(start);
+  mop3.Print();
 
-  sop4.extrapolateToPlane(middle);
-  //sop4.Print();
-  sop4.extrapolateToPlane(start);
-  sop4.Print();
+  mop4.extrapolateToPlane(middle);
+  //mop4.Print();
+  mop4.extrapolateToPlane(start);
+  mop4.Print();
 
   return 0;
 }
